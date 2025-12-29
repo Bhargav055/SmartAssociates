8ccb9884573937dd82a219d433e8c80afa361e85
Bhargav Seethamsetti <150264062+Bhargav055@users.noreply.github.com>
2025-12-29 21:08:25 +0530

    base

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-"homepage": "https://YOUR_USERNAME.github.io/smart-associates",
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: "/smart-associates/",
 });
